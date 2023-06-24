6ab0dfe3917ccfff062dd99e22a9ae76803e2888
nathangerken7 <126472627+nathangerken7@users.noreply.github.com>
2023-06-24 14:36:36 -0300

    Update q3.go

--- q3/q3.go
+++ q3/q3.go
@@ -5,5 +5,23 @@ package q3
 //A cada vez, você pode subir 1 ou 2 degraus. De quantas maneiras distintas você pode subir até o topo?
 
 func ClimbStairs(n int) int {
-	return 0
+	if n <= 1 {
+		return 1
+	} else {
+
+		var maneiras int
+
+		var seq = []int{}
+		seq = append(seq, 1)
+		seq = append(seq, 2)
+
+		for x := 2; x < n; x++ {
+			num := seq[x-1] + seq[x-2]
+			seq = append(seq, num)
+		}
+
+		maneiras = seq[n-1]
+
+		return maneiras
+	}
 }
